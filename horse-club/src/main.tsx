971fe2281079469eb64e2177b18ff76f213d20c2
Brian-Gitonga <kinaybrenda291@gmail.com>
2025-11-12 20:13:22 +0300

    removed some bugs

--- horse-club/src/main.tsx
+++ horse-club/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import Header from './components/header.tsx'
 import Hero from './components/hero.tsx'
 import AboutSection from './components/about-section.tsx'
@@ -13,7 +12,7 @@ import Footer from './components/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    
     <Header />
     <Hero />
     <AboutSection />
